771ef11f95a11b84500879ef55d53aa52771169a
Andy Mikhaylenko <neithere@gmail.com>
2014-12-10 04:57:08 +0500

    Add a simple Gramps XML parser/viewer

---
+++ simple_gramps_data_parser/show_people.py
@@ -0,0 +1,185 @@
+#!/usr/bin/env python
+
+from collections import OrderedDict
+import pprint
+
+import argh
+import xmltodict
+
+
+'''
+OrderedDict([
+    ('@type', 'Birth Name'),
+    ('first', 'Мария'),
+    ('surname', [
+        OrderedDict([
+            ('@derivation', 'Inherited'),
+            ('#text', 'Аузбикович')
+        ]),
+        OrderedDict([
+            ('@prim', '0'),
+            ('@derivation', 'Patronymic'),
+            ('#text', 'Ксаверьевна')
+        ])
+    ])
+])
+'''
+
+
+
+
+def TEST__etree(path):
+    from xml.etree.elementtree import ElementTree
+    tree = ElementTree(path)
+    for x in tree.findall('person'):
+        print(x)
+
+
+def _format_name(name_node):
+    template = '{primary} ({nonpatronymic}), {first} {patronymic}'
+
+    #print('---')
+    #import pprint
+    #pprint.pprint(name_node)
+    #print('---')
+
+    if isinstance(name_node, list):
+        # XXX this is naïve; we better filter out the `{'@alt': '1', ...}` ones
+        name_node = name_node[0]
+
+    first = name_node.get('first', '?')
+    primary_surnames = []
+    patronymic = []
+    nonpatronymic = []
+
+    surname_spec = name_node.get('surname', '?')
+
+    if isinstance(surname_spec, str):
+        #primary_surnames.append(surname_spec)
+        surname_spec = [
+            {
+                '#text': surname_spec,
+            }
+        ]
+
+    if isinstance(surname_spec, dict):
+        surname_spec = [surname_spec]
+
+
+    for surname in surname_spec:
+        #print('surname:', surname)
+        if isinstance(surname, str):
+            surname = {
+                '#text': surname,
+            }
+        derivation = surname.get('@derivation')
+        is_primary = surname.get('@prim') != '0'
+        text = surname.get('#text', '???')
+        if derivation == 'Patronymic':
+            patronymic.append(text)
+        elif is_primary:
+            primary_surnames.append(text)
+        else:
+            nonpatronymic.append(text)
+
+    ' '.join(primary_surnames)
+    ' '.join(patronymic)
+    ' '.join(nonpatronymic)
+    return template.format(
+        first = first,
+        primary = ' '.join(primary_surnames),
+        patronymic = ' '.join(patronymic),
+        nonpatronymic = ' '.join(nonpatronymic),
+    ).replace(' ()', '')
+
+
+def main(path):
+    def _postprocessor(path, key, value):
+        # simplify data by ignoring dict order
+        if isinstance(value, OrderedDict):
+            value = dict(value)
+        return key, value
+
+    with open(path) as f:
+        tree = xmltodict.parse(f.read(), postprocessor=_postprocessor)
+    db = tree['database']
+    people = db['people']['person']
+    events = db['events']['event']
+    families = db['families']['family']
+    print(len(people), 'people')
+    print(len(events), 'events')
+    print(len(families), 'families')
+    print('---')
+
+    for person in people:
+        #print('')
+        name = _format_name(person['name'])
+        #print('------------------')
+        print(person['@id'], person['gender'], name)
+        #pprint.pprint(person)
+        if 'childof' in person:
+            parents = _find_people(db, 'parentin', person['childof'])
+            print('  Parents:', ' + '.join(_format_name(p['name']) for p in parents))
+        if 'eventref' in person:
+            for event in _find_events(db, person['eventref']):
+                #pprint.pprint(event)
+                places = ', '.join(x['ptitle'] for x in _find_places(db, event.get('place')))
+                print('  {type_}: {date} {summary} @{place}'.format(
+                          date=_format_dateval(event.get('dateval')) or '(when?)',
+                          type_=event['type'],
+                          summary=event.get('description', ''),
+                          place=places or 'where?'))
+        #families = db['families']['family']
+#        pprint.pprint(family)
+
+
+def _find_people(db, key, searched_values):
+    searched_values = _ensure_list(searched_values)
+    searched_hlinks = [value['@hlink'] for value in searched_values]
+    for person in db['people']['person']:
+        if key not in person:
+            continue
+        elems = _ensure_list(person[key])
+        # {..., 'parentin': {'@hlink': '_ce35d5cb4ea5a05e4aceed3a3fa'}}
+        hlinks = [elem['@hlink'] for elem in elems]
+        if any(h in searched_hlinks for h in hlinks):
+            yield person
+
+
+def _find_by_handle(db, category_pl, category_sg, hlinks):
+    if not hlinks:
+        return
+    searched_values = _ensure_list(hlinks)
+    searched_hlinks = [value['@hlink'] for value in searched_values]
+    for event in db[category_pl][category_sg]:
+        #pprint.pprint(event)
+        if event['@handle'] in searched_hlinks:
+            yield event
+
+def _find_events(db, searched_values):
+    return _find_by_handle(db, 'events', 'event', searched_values)
+
+def _find_places(db, searched_values):
+    return _find_by_handle(db, 'places', 'placeobj', searched_values)
+
+
+def _ensure_list(item):
+    if isinstance(item, list):
+        return item
+    else:
+        return [item]
+
+
+def _format_dateval(dateval):
+    if not dateval:
+        return
+    vals = [
+        dateval.get('@quality'),
+        dateval.get('@type'),
+        dateval.get('@val'),
+    ]
+    return ' '.join([x for x in vals if x])
+
+
+if __name__ == '__main__':
+    argh.dispatch_command(main)
