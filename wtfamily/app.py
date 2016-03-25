0ee8af6da8296bfacbcdabef78cd01ea04455a0d
Andy Mikhaylenko <neithere@gmail.com>
2016-03-25 01:21:46 +0100

    Improve the interactive shell

--- wtfamily/app.py
+++ wtfamily/app.py
@@ -55,26 +55,76 @@ def main():
 
 
 def shell():
+    import atexit
     import code
+    import importlib
 
     # local
     import models
+    import storage
+
+    RELEVANT_MODULES = (
+        models,
+        storage,
+    )
 
     conf = _get_config()
     storage = Storage(conf['storage'])
 
-    # monkey-patch to avoid flask.g
-    models.Entity._get_root_storage = lambda: storage
+    def _reload_relevant_modules_but_keep_storage():
+        _reload_relevant_modules(keep_storage=True)
+
+    def _reload_relevant_modules(keep_storage=False):
+        try:
+            storage_obj = models.Entity._get_root_storage()
+        except RuntimeError:    # flask.g?
+            storage_obj = None
+
+        for m in RELEVANT_MODULES:
+            print('reloading', m, '...')
+            importlib.reload(m)
+
+        if not (storage_obj or keep_storage):
+            storage_obj = Storage(conf['storage'])
+
+        # monkey-patch to avoid flask.g
+        models.Entity._get_root_storage = lambda: storage_obj
+
+    _reload_relevant_modules()
 
     namespace = {
-        'storage': storage,
-        'models': models,
+        's': models.Entity._get_root_storage,
+        'm': models,
+        'r': _reload_relevant_modules_but_keep_storage,
+        'reload': _reload_relevant_modules_but_keep_storage,
+        'reload_full': _reload_relevant_modules,
     }
 
-    import rlcompleter
-    import readline
-    readline.set_completer(rlcompleter.Completer(namespace).complete)
-    readline.parse_and_bind("tab:complete")
+    for m in RELEVANT_MODULES:
+        for k, v in m.__dict__.items():
+            if isinstance(v, type) and v.__module__ == m.__name__:
+                namespace[k] = v
+
+    print('Locals:', list(namespace))
+
+
+    try:
+        import readline
+    except ImportError:
+        readline = None
+    if readline:
+        import rlcompleter
+        readline.set_completer(rlcompleter.Completer(namespace).complete)
+        readline.parse_and_bind("tab:complete")
+
+        # XXX hacky/debuggish
+        history_file = os.path.expanduser('~/.local/share/wtfamily/shell-history.log')
+        history_file_dir = os.path.dirname(history_file)
+        if not os.path.exists(history_file_dir):
+            os.makedirs(history_file_dir)
+        atexit.register(lambda: readline.write_history_file(history_file))
+        if os.path.exists(history_file):
+            readline.read_history_file(history_file)
 
     banner = '{} interactive shell'.format(APP_NAME)
     code.interact(banner=banner, local=namespace)
