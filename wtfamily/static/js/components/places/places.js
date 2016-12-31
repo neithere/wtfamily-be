57b7149b779e22a2ffe3d87605f4cd90d6cd021b
Andy Mikhaylenko <neithere@gmail.com>
2016-12-31 05:25:33 +0100

    Finalize persistent obj selection for Places

--- wtfamily/static/js/components/places/places.js
+++ wtfamily/static/js/components/places/places.js
@@ -18,16 +18,7 @@ define([
                     var query = this.attr('filterQuery');
                     return Place.findAllSorted({
                         q: query
-                    }).done(function(results) {
-                        // select on of them according to the active route
-                        var objId = can.route.attr('objId');
-                        var obj;
-                        if (_.isEmpty(objId)) {
-                            return null;
-                        }
-                        obj = _.find(results, {id: objId});
-                        this.attr('selectedObject', obj);
-                    }.bind(this));
+                    });
                 },
             },
             relatedEvents: {
@@ -39,14 +30,35 @@ define([
         },
         selectObject: function(obj, elems, event) {
             can.route.attr('objId', obj.id);
-            // TODO: do this via route event handler?
-            this.attr('selectedObject', obj);
+            // We could directly assign the attr here, but in order to handle
+            // both sources of object selection (route and click), we merely
+            // change the route and let the component detect the route event.
+            //this.attr('selectedObject', obj);
         },
+        selectObjectById: function(objId) {
+            if (_.isEmpty(objId)) {
+                return
+            }
+            this.attr('object_list').done(function(results) {
+                var obj = _.find(results, {id: objId});
+                this.attr('selectedObject', obj);
+            }.bind(this));
+        }
     });
 
     can.Component.extend({
         tag: 'wtf-places',
         viewModel: PlaceViewModel,
-        template: can.view('app/components/places/places')
+        template: can.view('app/components/places/places'),
+        init: function() {
+            // select an item according to the active route
+            var objId = can.route.attr('objId');
+            this.viewModel.selectObjectById(objId);
+        },
+        events: {
+            '{can.route} change': function(data) {
+                this.viewModel.selectObjectById(data.attr('objId'));
+            }
+        }
     });
 });
